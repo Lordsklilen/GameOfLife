5257e206262f96741f70d2551b96334e23acdec7
Michał Dela <dela_m@interia.pl>
2019-03-22 15:34:01 +0100

    Saving properly rle file

--- CLRWindowOutput/GameEngine/RLEHelper.cpp
+++ CLRWindowOutput/GameEngine/RLEHelper.cpp
@@ -11,6 +11,48 @@ RLEHelper::~RLEHelper()
 void RLEHelper::SaveFile(shared_ptr <RLEstorage> storage) {
 	
 	ofstream outfile;
-	outfile.open("C:\\Users\\Dela_\\source\\repos\\GameOfLife\\x64\\Debug\\elo.csv");
-	outfile << "test";
+
+
+
+	outfile.open("C:\\Users\\Dela_\\source\\repos\\GameOfLife\\x64\\Debug\\temp.rle");
+	auto stream = CreateOutputStream(storage).str();
+	outfile << stream;
+	outfile.close();
+}
+
+ostringstream RLEHelper::CreateOutputStream(shared_ptr <RLEstorage> storage) {
+
+	std::ostringstream ss;
+	ss << "x = " << storage->x << ", y = " << storage->y << "\n";
+	char dead = 'b';
+	char alive = 'o';
+	for each (auto row in storage->blockBoard)
+	{
+		char currentChar = 'n'; 
+		int counter= 0;
+		for each (auto cell in row)
+		{
+			counter++;
+			if (currentChar=='n') {
+				if (cell.isAlive)
+					currentChar = alive;
+				else
+					currentChar = dead;
+			}
+			
+			if (cell.isAlive && currentChar == 'b') {
+				ss << dead << counter;
+				currentChar = alive;
+				counter = 0;
+			}
+			else if (!cell.isAlive && currentChar == 'o') {
+				ss << alive << counter;
+				currentChar = dead;
+				counter = 0;
+			}
+		}
+		ss << currentChar << counter;
+		ss << "$\n";
+	}
+	return ss;
 }
--- CLRWindowOutput/GameEngine/RLEHelper.h
+++ CLRWindowOutput/GameEngine/RLEHelper.h
@@ -4,12 +4,14 @@
 #include <fstream>
 #include <experimental/filesystem>
 #include <filesystem>
+#include <sstream>
 #include "RLEstorage.h"
 using namespace std::experimental::filesystem::v1;
 
 using namespace std;
 class RLEHelper
 {
+	ostringstream CreateOutputStream(shared_ptr <RLEstorage> storage);
 public:
 	void SaveFile(shared_ptr <RLEstorage> storage);
 	shared_ptr <RLEstorage>LoadFile(string path);
